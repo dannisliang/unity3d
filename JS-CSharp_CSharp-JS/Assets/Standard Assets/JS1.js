999c4966f03a17007e3a549332f6544f4a140c91
Icaro Magalhaes <icaro.lmag@gmail.com>
2015-07-01 18:12:35 -0300

    deletes old folder

--- JS-CSharp_CSharp-JS/Assets/Standard Assets/JS1.js
+++
@@ -1,4 +0,0 @@
-/* Unity3D: JavaScript->C# or C#->JavaScript access */
-
-//This variable will be accessed through C#
-public var message : String = "This is a JavaScript code being accessed trough C#.";
